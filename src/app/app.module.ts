ea4962d6207d5c4bc8608daee30565b5e73eae7e
FabrizzioRuiz <fabriruiz18@hotmail.com>
2020-10-06 18:59:09 +0200

    Doc de modulos y componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,19 +15,28 @@ import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './formulario/formulario.component';
 
 
-
+// Los decoradores son funciones que modifican clases de JavaScript.
 @NgModule({
+  // Las declaraciones son las clases de vista que pertenecen a este módulo.
   declarations: [
     AppComponent,
     PersonasComponent,
     FormularioComponent
   ],
+
+  // Importamos otros módulos cuyas clases exportadas sean necesarias para las
+  // plantillas de componentes declaradas en este módulo.
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
+
+  // Los proveedores son los creadores de servicios que este módulo contribuye a la
+  // recolección global de servicios; Se vuelven accesibles en todas las partes de la
+  // aplicación. Esto lo hablitamos cuando invocamos servicios desde una base de datos.
   providers: [PersonaService, DataService],
   bootstrap: [AppComponent]
 })
